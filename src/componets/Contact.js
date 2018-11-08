01109e94427fedab948726202cdeeb32b7b71c41
Chathuranga Jayanidu <chathuranga.jayanidu@gmail.com>
2018-11-08 12:45:26 +0530

    changing state within a component

--- src/componets/Contact.js
+++ src/componets/Contact.js
@@ -2,27 +2,38 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
-  state = {};
-  onShowClick = (name, e) => {
-    console.log(e.target);
-    console.log(name);
+  state = {
+    showContactInfo: false
   };
+
+  // onShowClick = e => {
+  //   this.setState({
+  //     showContactInfo: !this.state.showContactInfo
+  //   });
+  // };
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
         <h4>
+          {/* {name} <i onClick={this.onShowClick} className="fas fa-sort-down" /> */}
           {name}{" "}
           <i
-            onClick={this.onShowClick.bind(this, name)}
+            onClick={() => {
+              this.setState({ showContactInfo: !this.state.showContactInfo });
+            }}
             className="fas fa-sort-down"
           />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
