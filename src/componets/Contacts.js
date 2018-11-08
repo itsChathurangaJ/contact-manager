222a807c349ddcf9d46638b0a5f31d2cec10e30c
Chathuranga Jayanidu <chathuranga.jayanidu@gmail.com>
2018-11-08 12:00:32 +0530

    events | passing data to third party methods | used bind

--- src/componets/Contacts.js
+++ src/componets/Contacts.js
@@ -2,31 +2,28 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "Chathu J",
-          email: "cj@gmial.com",
-          phone: "0779733078"
-        },
-        {
-          id: 2,
-          name: "Amma",
-          email: "mother@gmial.com",
-          phone: "07673983120"
-        },
-        {
-          id: 3,
-          name: "Thaththa",
-          email: "father@gmial.com",
-          phone: "0777383120"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Chathu J",
+        email: "cj@gmial.com",
+        phone: "0779733078"
+      },
+      {
+        id: 2,
+        name: "Amma",
+        email: "mother@gmial.com",
+        phone: "07673983120"
+      },
+      {
+        id: 3,
+        name: "Thaththa",
+        email: "father@gmial.com",
+        phone: "0777383120"
+      }
+    ]
+  };
 
   render() {
     const { contacts } = this.state;
